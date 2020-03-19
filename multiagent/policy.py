9b100ec1c26a3cc0ff56beb02b57d09242271a3f
ngrupen <nag83@cornell.edu>
2020-03-19 12:56:47 -0400

    cleanup updated policies, move interactive policy back to multiagent envs

--- multiagent/policy.py
+++ multiagent/policy.py
@@ -17,9 +17,15 @@ class InteractivePolicy(Policy):
         # hard-coded keyboard events
         self.move = [False for i in range(4)]
         self.comm = [False for i in range(env.world.dim_c)]
+        self.discrete = self.env.discrete_action_space
+
         # register keyboard events with this environment's window
-        env.viewers[agent_index].window.on_key_press = self.key_press
-        env.viewers[agent_index].window.on_key_release = self.key_release
+        if env.shared_viewer:
+            env.viewers[0].window.on_key_press = self.key_press
+            env.viewers[0].window.on_key_release = self.key_release
+        else:
+            env.viewers[agent_index].window.on_key_press = self.key_press
+            env.viewers[agent_index].window.on_key_release = self.key_release
 
     def action(self, obs):
         # ignore observation and just act based on keyboard events
@@ -29,7 +35,7 @@ class InteractivePolicy(Policy):
             if self.move[1]: u = 2
             if self.move[2]: u = 4
             if self.move[3]: u = 3
-        else:
+        elif self.discrete: 
             u = np.zeros(5) # 5-d because of no-move action
             if self.move[0]: u[1] += 1.0
             if self.move[1]: u[2] += 1.0
@@ -37,16 +43,24 @@ class InteractivePolicy(Policy):
             if self.move[2]: u[4] += 1.0
             if True not in self.move:
                 u[0] += 1.0
-        return np.concatenate([u, np.zeros(self.env.world.dim_c)])
+        else:
+            u = np.zeros(2)
+            if self.move[0]: u[0] += 1.0
+            if self.move[1]: u[0] -= 1.0
+            if self.move[3]: u[1] += 1.0
+            if self.move[2]: u[1] -= 1.0
+
+        return np.array([u, np.zeros(self.env.world.dim_c)])
 
     # keyboard event callbacks
     def key_press(self, k, mod):
-        if k==key.LEFT:  self.move[0] = True
-        if k==key.RIGHT: self.move[1] = True
-        if k==key.UP:    self.move[2] = True
-        if k==key.DOWN:  self.move[3] = True
+        if k==key.RIGHT: self.move[0] = True
+        if k==key.LEFT:  self.move[1] = True
+        if k==key.DOWN:  self.move[2] = True
+        if k==key.UP:    self.move[3] = True
+
     def key_release(self, k, mod):
-        if k==key.LEFT:  self.move[0] = False
-        if k==key.RIGHT: self.move[1] = False
-        if k==key.UP:    self.move[2] = False
-        if k==key.DOWN:  self.move[3] = False
+        if k==key.RIGHT: self.move[0] = False
+        if k==key.LEFT:  self.move[1] = False
+        if k==key.DOWN:  self.move[2] = False
+        if k==key.UP:    self.move[3] = False
