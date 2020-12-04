6a40a63a01f6524f048542fa187b7eb7f5702ebd
ngrupen <nag83@cornell.edu>
2020-12-04 16:00:35 -0500

    shorten length of predator prey game

--- multiagent/scenarios/simple_torus_comm.py
+++ multiagent/scenarios/simple_torus_comm.py
@@ -153,7 +153,7 @@ class Scenario(BaseScenario):
                 for a in adversaries:
                     if self.is_collision(a, agent):
                         agent.captured = True 
-                        rew -= 50
+                        rew -= 25
                         break
             return rew
         else:
@@ -177,7 +177,7 @@ class Scenario(BaseScenario):
                         capture_idxs.append(i)
                         ag.captured = True 
 
-            rew += 50 * len(set(capture_idxs))
+            rew += 25 * len(set(capture_idxs))
         return rew
 
     def terminal(self, agent, world):
