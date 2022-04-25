fix softmax for pnp rbfs too

multiagent/scenarios/simple_torus.py
@@ -233,74 +233,9 @@ class Scenario(BaseScenario):
 
             obs = np.concatenate([agent.state.p_pos] + other_pos)
             return obs
-        
+
         elif world.obs_type == 'rbf':
             obs_vec = []
-            obs_map_preds = np.zeros((world.obs_dims, world.obs_dims))
-            obs_map_prey = np.zeros((world.obs_dims, world.obs_dims))
-
-            eps_pred = 1.5
-            eps_prey = 1.5
-            w_pred = 0.45
-            w_prey = 0.45
-
-            idxs = np.indices((world.obs_dims, world.obs_dims))
-            idxs = np.flip(idxs, axis=(0,1))
-            idxs_flat = np.reshape(idxs, (2, world.obs_dims*world.obs_dims))
-            idxs_flat = np.swapaxes(idxs_flat, 0, 1)
-
-            for i, other in enumerate(world.agents):
-                if other is agent: continue
-
-                # full observations
-                other_pos = other.state.p_pos * world.bin_scale
-                # binned_pos = np.digitize(other_pos, world.obs_bins) - 1
-
-                # toroidal distance
-                dist = idxs_flat - other_pos
-                dist = (dist > world.obs_dims/2) * -world.obs_dims + dist
-                dist = (dist < -world.obs_dims/2) * world.obs_dims + dist
-                dist = np.sqrt(np.sum(np.abs(dist)**2, axis=1)) # euclidean distance
-                dist = np.reshape(dist, (world.obs_dims, world.obs_dims))
-
-                if other.adversary:
-                    # if agent.id == 0:
-                        # print('teammate pos = {}, scaled pos = {}, binned pos = {}'.format(other.state.p_pos, other_pos, binned_pos))
-                    pf = np.exp(-(eps_pred*dist)**2)   
-                    obs_map_preds += w_pred * pf
-                else:
-                    # if agent.id == 0:
-                        # print('prey pos = {}, binned pos = {}'.format(other_pos, binned_pos))
-                    pf = np.exp(-(eps_prey*dist)**2)   
-                    obs_map_prey += w_prey * pf
-
-                # keep raw positions around
-                obs_vec.append(other.state.p_pos)
-
-            # current agent relative prey (scaled)
-            agent_pos = agent.state.p_pos
-            # agent_pos = agent_pos / world.size
-
-            # relative pos
-            prey_pos = obs_vec[-1]
-            agent_pos = agent_pos - prey_pos
-            agent_pos = (agent_pos > world.size/2) * -world.size + agent_pos
-            agent_pos = (agent_pos < -world.size/2) * world.size + agent_pos
-            # agent_pos = agent_pos / (world.size / 2)
-
-            # print('Agent {}'.format(agent.id))
-            # np.set_printoptions(linewidth=2500, suppress=True, precision=3, threshold=10000)
-            # if agent.id == 0:
-                # print('pred pos = {}'.format(agent_pos))
-                # print('obs map pred = \n{}\n'.format(obs_map_preds))
-                # print('obs map prey = \n{}\n'.format(obs_map_prey))
-
-            obs = np.stack([obs_map_preds, obs_map_prey])
-            return (obs, np.concatenate([agent.state.p_pos] + obs_vec))
-
-        elif world.obs_type == 'rbf_mlp':
-            obs_vec = []
-            obs_map_preds = np.zeros((world.obs_dims, world.obs_dims))
             obs_map_prey = np.zeros((world.obs_dims, world.obs_dims))
 
             # for regular
@@ -309,12 +244,6 @@ class Scenario(BaseScenario):
             w_pred = 0.45
             w_prey = 0.45
 
-            # for softmax
-            # eps_pred = 0.75
-            # eps_prey = 0.75
-            # w_pred = 0.95
-            # w_prey = 0.95
-
             threshold = world.obs_dims / 4.
 
             idxs = np.indices((world.obs_dims, world.obs_dims))
@@ -322,12 +251,12 @@ class Scenario(BaseScenario):
             idxs_flat = np.reshape(idxs, (2, world.obs_dims*world.obs_dims))
             idxs_flat = np.swapaxes(idxs_flat, 0, 1)
 
+            pred_maps = []
             for i, other in enumerate(world.agents):
                 if other is agent: continue
 
                 # full observations
                 other_pos = other.state.p_pos * world.bin_scale
-                # binned_pos = np.digitize(other_pos, world.obs_bins) - 1
 
                 # toroidal distance
                 dist = idxs_flat - other_pos
@@ -339,12 +268,11 @@ class Scenario(BaseScenario):
                 if other.adversary:
                     # if agent.id == 0:
                         # print('teammate pos = {}, scaled pos = {}, binned pos = {}'.format(other.state.p_pos, other_pos, binned_pos))
-                    pf = np.exp(-(eps_pred*dist)**2)
-
+                    
                     # threshold pf by distance
+                    pf = np.exp(-(eps_pred*dist)**2)
                     pf = (dist < threshold) * pf
-
-                    obs_map_preds += w_pred * pf
+                    pred_maps.append(w_pred * pf)
                 else:
                     # if agent.id == 0:
                         # print('prey pos = {}, binned pos = {}'.format(other_pos, binned_pos))
@@ -357,6 +285,19 @@ class Scenario(BaseScenario):
                 
                 # keep raw positions around
                 obs_vec.append(other.state.p_pos)
+
+            if len(pred_maps) > 0:
+                # normal ghosts
+                # no softmax
+                # obs_map_preds = np.sum(pred_maps, axis=0)
+
+                # for softmax
+                obs_map_preds = np.stack(pred_maps)
+                obs_map_preds = np.max(obs_map_preds, axis=0)
+            else:
+                # no normal ghosts
+                obs_map_preds = np.zeros((self.layout.height, self.layout.width))
+
 
             # current agent relative prey (scaled)
             agent_pos = agent.state.p_pos
@@ -375,11 +316,6 @@ class Scenario(BaseScenario):
 
             # for regular
             obs = np.concatenate([agent_pos, np.ravel(obs_map_preds), np.ravel(obs_map_prey)])
-
-            # for softmax
-            # obs = softmax(np.stack([obs_map_preds, obs_map_prey]))
-            # obs = np.concatenate([agent_pos, np.ravel(obs)])
-
             return (obs, np.concatenate([agent.state.p_pos] + obs_vec))
         else:
             return None