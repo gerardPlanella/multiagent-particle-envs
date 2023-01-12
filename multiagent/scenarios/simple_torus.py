5b953966ad97ad49c6e967c8aaf8e353866d1d98
gerardPlanella <gerard.plfo@gmail.com>
2023-01-12 16:10:01 +0100

    changed collaborative attribute assignment

--- multiagent/scenarios/simple_torus.py
+++ multiagent/scenarios/simple_torus.py
@@ -27,7 +27,7 @@ class Scenario(BaseScenario):
         world.shape = config.rew_shape
         world.predator_colors = COLOR_SCHEMES[config.pred_colors]
         world.tax = 0.0
-        world.collaborative = config.collaborative
+        world.collaborative = config.collaborative if hasattr(config, 'collaborative') else False 
 
         print('world size = {}'.format(world.size))
         print('num preds = {}'.format(config.n_preds))
